5f05f2c93f8d86ec6d9b667e99889073005cb0fb
Sergey9247 <Sammakarov@list.ru>
2023-03-28 17:03:45 +0300

    add Override

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,7 +13,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
-
+    @Override
     public void createUsersTable() {
         String createTable = "CREATE TABLE IF NOT EXISTS users (Id INT PRIMARY KEY AUTO_INCREMENT, " +
                 "name VARCHAR(20), lastName VARCHAR(20), age INT)";
@@ -24,7 +24,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
 
     }
-
+    @Override
     public void dropUsersTable() {
         String drop = "DROP TABLE IF EXISTS users";
         try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(drop)) {
@@ -34,7 +34,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
 
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         String save = "INSERT INTO USERS (name, lastName, age) VALUES (?, ?, ?)";
 
@@ -47,7 +47,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
             e.printStackTrace();
         }
     }
-
+    @Override
     public void removeUserById(long id) {
         String remove = "DELETE FROM users WHERE id = ?";
 
@@ -59,7 +59,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
 
     }
-
+    @Override
     public List < User > getAllUsers() {
         String all = "select * from users";
         List < User > users = new ArrayList < > ();
@@ -75,7 +75,7 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
         return users;
     }
-
+    @Override
     public void cleanUsersTable() {
         String clean = "TRUNCATE TABLE users";
         try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(clean)) {
